44102dbd29e46b6b5dd4380a549446a4756fab5e
mahendray-asu <mahendray@advsysunlimited.com>
2020-06-18 09:23:36 +0530

    Initial Commit

---
+++ OCR_POC_AYM/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  OCR_POC_AYM
+//
+//  Created by Apple on 18/06/20.
+//  Copyright © 2020 Apple. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
